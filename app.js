952c10fd6b63487bab3af9317bb78d6828785195
fulltechstack <fulltechstack@gmail.com>
2023-04-30 01:06:49 +0530

    changes

--- app.js
+++ app.js
@@ -3,6 +3,11 @@ const express = require('express');
   
 const app = express();
 const PORT = 9080;
+
+app.use('/',(req,res)=>{
+    console.log(res);
+    res.send('working successfully');
+});
   
 app.listen(PORT, (error) =>{
     if(!error)
